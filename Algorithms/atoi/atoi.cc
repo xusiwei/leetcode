f8d09afc8e3d575a44c22370638f86589e63c2bd
Xu <xusiwei1236@163.com>
2015-06-02 14:29:13 +0800

    add copyright notice

--- Algorithms/atoi/atoi.cc
+++ Algorithms/atoi/atoi.cc
@@ -1,4 +1,6 @@
 /*
+copyright xu(xusiwei1236@163.com), all right reseved.
+
 Implement atoi to convert a string to an integer.
 
 Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
