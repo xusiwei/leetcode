6fd6628649471a101644b574bcb34c9063d4f983
Xu Siwei <xusiwei1236@163.com>
2015-08-11 20:25:05 +0800

    add Roman to Integer problem

---
+++ Algorithms/RomanToInteger/roman2int.cc
@@ -0,0 +1,56 @@
+/*
+copyright xu(xusiwei1236@163.com), all right reserved.
+
+Roman to Integer
+====================
+
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+static int value[256];
+
+class Solution {
+public:
+    Solution() {
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+    }
+
+	// I  II  III  IV  V  VI  VII  VIII  IX  X
+	// XI XII XIII XIV XV XVI XVII XVIII XIX XX
+    int romanToInt(string s) {
+        if(s.length() == 0) return 0;
+        int res = 0;
+        for (int i=0; i<s.length(); ++i) {
+            int v = value[s[i]]; // current value
+            if(i < s.length()-1) { // not last char
+                int nv = value[s[i+1]]; // next value
+                if(v >= nv) res += v;
+                else {
+                    res += (nv - v); // IV, IX, etc.
+                    ++i;
+                }
+            }
+            else res += v; // last
+        }
+        return res;
+    }
+};
+
+int main()
+{
+	string roman;
+	while(cin >> roman) {
+		cout << roman << ": " << Solution().romanToInt(roman) << endl;
+	}
+}
--- README.md
+++ README.md
@@ -76,7 +76,7 @@ If you have any ideas about my solution, welcome to post a comment.
 | 10  | [Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching/)  | [C](Algorithms/RegularExpressionMatch/reMatch.c) | Hard | [Dynamic Programming][dp], [Backtracking][bt], [String][string] |
 | 11  | [Container With Most Water](https://leetcode.com/problems/container-with-most-water/)  | [C++](Algorithms/ContainMostWater/containMostWater.cc) | Medium | [Array][array], [Two Pointers][pointer] |
 | 12  | Integer to Roman  |  | Medium |
-| 13  | Roman to Integer  |  | Easy |
+| 13  | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/)  | [C++](Algorithms/RomanToInteger/roman2int.cc) | Easy | [String][string], [Math][math] |
 | 14  | Longest Common Prefix  |  | Easy |
 | 15  | 3Sum  |  | Medium |
 | 16  | 3Sum Closest  |  | Medium |
