61cb1b06458bab0708f1395160faf93df706bf6a
xusiwei1236 <xusiwei1236@163.com>
2015-08-14 14:48:55 +0800

    formats indents and remove unused code

--- Algorithms/3Sum/3sum.cc
+++ Algorithms/3Sum/3sum.cc
@@ -28,45 +28,6 @@ using namespace std;
 #define trace
 #endif
 
-#if 0
-
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        unordered_set<string> dict;
-        vector<vector<int>> result;
-        
-        multimap<int, tuple<int, int>> buf;
-        for(int i = 0, sz = nums.size(); i < sz-1; ++i) {
-            auto it = buf.find(-nums[i]);
-            if(it != buf.end()) {
-                for(; it->first + nums[i] == 0; it++) {
-                    int a = std::get<0>(it->second);
-                    int b = std::get<1>(it->second);
-                    
-                    multiset<int> t{nums[a], nums[b], nums[i]};
-                
-                    string s;
-                    for(int n: t) s = s + (n + " ");
-                    
-                    auto pos = dict.find(s);
-                    if(pos == dict.end()) {
-                        dict.insert(pos, s);
-                        result.push_back(vector<int>(t.begin(), t.end()));
-                    }
-                }
-            }
-            for(int j = i + 1; j < sz; ++j) {
-                int sum = nums[i] + nums[j];
-                buf[sum] = make_tuple(i, j);
-            }
-        }
-        return result;
-    }
-};
-
-#else
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -75,58 +36,56 @@ public:
         std::sort(nums.begin(), nums.end());
         for(int i = 0, sz = nums.size(); i < sz-1; ++i) {
             int target = -nums[i];
-			int front = i+1;
-			int back = sz-1;
-			
-			// find: nums[front] + nums[back] == sum
-			while(front < back) {
-				int sum = nums[front] + nums[back];
-				if(sum == target){
-					vector<int> t(3, 0);
-					t[0] = nums[i];
-					t[1] = nums[front];
-					t[2] = nums[back];
-					
-					result.push_back(t);
-					
-					// for duplicate Number 2, 3.
-					while(front < back && nums[front] == t[1]) front++;
-					while(front < back && nums[back] == t[2]) back--;
-				}
-				else if(sum < target) {
-					while(front < back && nums[front+1] == t[front]) front++;
-					front++;
-				}
-				else { // if(sum > target)
-					while(front < back && nums[back-1] == t[back]) back--;
-					back--;
-				}
-			}
-			
-			// for duplicate Number 1.
-			while(i+1 < sz && nums[i+1] == nums[i]) i++;
+            int front = i+1;
+            int back = sz-1;
+            
+            // find: nums[front] + nums[back] == sum
+            while(front < back) {
+                int sum = nums[front] + nums[back];
+                if(sum == target){
+                    vector<int> t(3, 0);
+                    t[0] = nums[i];
+                    t[1] = nums[front];
+                    t[2] = nums[back];
+                    
+                    result.push_back(t);
+                    
+                    // for duplicate Number 2, 3.
+                    while(front < back && nums[front] == t[1]) front++;
+                    while(front < back && nums[back] == t[2]) back--;
+                }
+                else if(sum < target) {
+                    while(front < back && nums[front+1] == t[front]) front++;
+                    front++;
+                }
+                else { // if(sum > target)
+                    while(front < back && nums[back-1] == t[back]) back--;
+                    back--;
+                }
+            }
+            
+            // for duplicate Number 1.
+            while(i+1 < sz && nums[i+1] == nums[i]) i++;
         }
         return result;
     }
 };
 
-#endif
-
 int main()
 {
-	vector<int> nums; // {-1, 0, 1, 2, -1, -4};
-	
-	int n;
-	while(cin >> n) {
-		nums.push_back(n);
-	}
-	
-	auto sums = Solution().threeSum(nums);
-	
-	for(auto v : sums){
-		cout << "{";
-		for(int i: v) cout << i << ",";
-		cout << "}\n";
-	}
-	return 0;
+    vector<int> nums; // {-1, 0, 1, 2, -1, -4};
+    
+    int n;
+    while(cin >> n) {
+        nums.push_back(n);
+    }
+    
+    auto sums = Solution().threeSum(nums);
+    
+    for(auto v : sums){
+        cout << "{";
+        for(int i: v) cout << i << ",";
+        cout << "}\n";
+    }
+    return 0;
 }
