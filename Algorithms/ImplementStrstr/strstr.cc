98ec2f4eaf8888ee3c9f4a48931bdb7ba6686942
Xu Siwei <xusiwei1236@163.com>
2015-08-16 01:05:31 +0800

    add problem description and copyright

--- Algorithms/ImplementStrstr/strstr.cc
+++ Algorithms/ImplementStrstr/strstr.cc
@@ -1,3 +1,17 @@
+/*
+copyright xu(xusiwei1236@163.com), all right reserved.
+
+Implement strStr()
+===================
+
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Update (2014-11-02):
+The signature of the function had been updated to return the index instead of the pointer. If you still see your function signature returns a char * or String, please click the reload button to reset your code definition. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
