9b8c57a0baed8ef7dd6e2686d619836260557f2f
Xu Siwei <xusiwei1236@163.com>
2015-08-16 11:32:44 +0800

    replace TAB with 4-SPACE

--- Algorithms/DivideTwoIntegers/divde.c
+++ Algorithms/DivideTwoIntegers/divde.c
@@ -23,7 +23,7 @@ int divide(int num, int divisor) {
     unsigned unum, udiv, acc, factor;
     
     if(num == divisor) return 1;
-	if(divisor == INT_MIN) return 0;
+    if(divisor == INT_MIN) return 0;
     if(divisor == 0 || (num == INT_MIN && divisor == -1)) return INT_MAX;
     
     unum = num < 0     ? -num     : num;
@@ -33,34 +33,34 @@ int divide(int num, int divisor) {
     acc = udiv;
     factor = 1;
     while((acc<<1) < unum) { // && (acc<<1) < (UINT_MAX<<2u)
-    	acc = (acc<<1);
-    	factor <<= 1;
+        acc = (acc<<1);
+        factor <<= 1;
     }
     
     while(unum >= udiv) {
-    	unum -= acc;
-    	result += factor;
-    	trace("$ %d %d, %d %d\n", num, result, acc, factor);
-    	while(unum < acc) {
-    		acc >>= 1;
-    		factor >>= 1;
-    	}
+        unum -= acc;
+        result += factor;
+        trace("$ %d %d, %d %d\n", num, result, acc, factor);
+        while(unum < acc) {
+            acc >>= 1;
+            factor >>= 1;
+        }
     }
     return sign ? -result : result;
 }
 
 int main()
 {
-	int n, d, r;
-	
-	while(scanf("%d%d", &n, &d) == 2) {
-		r = divide(n, d);
-		printf("div(%d,%d): %d\n", n, d, r);
-		
-		printf("    %d/%d : ", n, d);
-		if((INT_MIN == n && -1 == d) || 0 == d)
-			printf("%s\n", "overflow");
-		else printf("%d\n", n/d);
-	}
-	return 0;
+    int n, d, r;
+    
+    while(scanf("%d%d", &n, &d) == 2) {
+        r = divide(n, d);
+        printf("div(%d,%d): %d\n", n, d, r);
+        
+        printf("    %d/%d : ", n, d);
+        if((INT_MIN == n && -1 == d) || 0 == d)
+            printf("%s\n", "overflow");
+        else printf("%d\n", n/d);
+    }
+    return 0;
 }
