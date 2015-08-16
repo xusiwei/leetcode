e7668c68a21b6b85834d76fd91692645ff180277
Xu Siwei <xusiwei1236@163.com>
2015-08-16 13:43:18 +0800

    add problem title

--- Algorithms/PalindromeNumber/palindromeNumber.cc
+++ Algorithms/PalindromeNumber/palindromeNumber.cc
@@ -1,6 +1,9 @@
 /*
 copyright xu(xusiwei1236@163.com), all right reserved.
 
+Palindrome Number
+==================
+
 Determine whether an integer is a palindrome. Do this without extra space.
 
 click to show spoilers.
