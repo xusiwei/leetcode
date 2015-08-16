a0e23c7bf5921a205b9ea1e7b3c0779fe5ce7f35
Xu Siwei <xusiwei1236@163.com>
2015-08-16 20:04:42 +0800

    pretty format and comments

--- Algorithms/SubstringWithConcatenationOfAllWords/substrWithCatAllWord.cc
+++ Algorithms/SubstringWithConcatenationOfAllWords/substrWithCatAllWord.cc
@@ -31,8 +31,8 @@ void printVector(Vector v);
 
 class Solution {
 public:
-	// foreach Concatenation of All Words, determine wether it is a substring of s.
-	// TLE(Time Limit Exceeded)
+    // foreach Concatenation of All Words, determine wether it is a substring of s.
+    // TLE(Time Limit Exceeded)
     vector<int> findSubstringBF(string s, vector<string>& words) {
         const int size = words.size();
         
@@ -47,7 +47,7 @@ public:
             for(int i = 0; i < size; ++i) ss += words[idx[i]];
             
             trace("ss: %s\n", ss.c_str());
-            const char* m = strstr(s.c_str(), ss.c_str());
+            const char* m = strstr(s.c_str(), ss.c_str()); // REPEATED WORK !!!!
             if(m) result.push_back(m - s.c_str());
         }while(next_permutation(idx.begin(), idx.end()));
         
@@ -61,47 +61,47 @@ public:
         vector<int> result;
         if(words.empty() || s.length() < words[0].length()) return result;
 
-		//counting words:
-		// in target substring,
-		// the total occurence of each word need same as dict.
-		map<string, int> dict;
-		for(int i = 0; i < size; ++i) {
-			dict[words[i]]++;
-		}
-		
-		//foreach substring:
-		// length of target substring == words[0].length() * words.size()
-		const int slen = s.length();
-		const int wlen = words[0].length();
-		const int tlen = wlen * size; // length of target substring.
-		for(int start = 0; start + tlen <= slen; start++) {
-			string ss = s.substr(start, tlen);
-			
-			// check wether it is cat by all words.
-			int total = 0;
-			map<string, int> matches;
-			for(int i = 0; i + wlen <= tlen; i += wlen) {
-				string w = ss.substr(i, wlen); // cut down a word.
-				auto pos = dict.find(w);
-				if(pos != dict.end()) { // found w in dict.
-					matches[w]++;
-					total++;
-				}
-				else { // not found
-					break;
-				}
-			}
-			if(size == total && matches.size() == dict.size()) {
-				auto it1 = dict.begin(), it2 = matches.begin();
-				while(it1 != dict.end() && it2 != matches.end()) {
-					if(it1->second != it2->second) break;
-					it1++; it2++;
-				}
-				if(it1 == dict.end()) result.push_back(start);
-			}
-		}
-		
-		return result;
+        //counting words:
+        // in target substring,
+        // the total occurence of each word need same as dict.
+        map<string, int> dict;
+        for(int i = 0; i < size; ++i) {
+            dict[words[i]]++;
+        }
+        
+        //foreach substring:
+        // length of target substring == words[0].length() * words.size()
+        const int slen = s.length();
+        const int wlen = words[0].length();
+        const int tlen = wlen * size; // length of target substring.
+        for(int start = 0; start + tlen <= slen; start++) {
+            string ss = s.substr(start, tlen);
+            
+            // check wether it is cat by all words.
+            int total = 0;
+            map<string, int> matches;
+            for(int i = 0; i + wlen <= tlen; i += wlen) {
+                string w = ss.substr(i, wlen); // cut down a word.
+                auto pos = dict.find(w);
+                if(pos != dict.end()) { // found w in dict.
+                    matches[w]++;
+                    total++;
+                }
+                else { // not found
+                    break;
+                }
+            }
+            if(size == total && matches.size() == dict.size()) {
+                auto it1 = dict.begin(), it2 = matches.begin();
+                while(it1 != dict.end() && it2 != matches.end()) {
+                    if(it1->second != it2->second) break;
+                    it1++; it2++;
+                }
+                if(it1 == dict.end()) result.push_back(start);
+            }
+        }
+        
+        return result;
     }
 };
 
