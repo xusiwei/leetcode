c99853719e818d356a033f4faa3842d4d513ff07
Xu Siwei <xusiwei1236@163.com>
2015-08-16 13:34:12 +0800

    add problem title

--- Algorithms/MergeTwoSortedList/mergeTwoSortedList.cc
+++ Algorithms/MergeTwoSortedList/mergeTwoSortedList.cc
@@ -1,6 +1,9 @@
 /*
 copyright xu(xusiwei1236@163.com), all right reserved.
 
+Merge Two Sorted Lists
+=======================
+
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 */
 
