caa9d59c0d0cedd77f10f56e91d495f94b50dfae
Xu Siwei <xusiwei1236@163.com>
2015-08-16 13:36:52 +0800

    add problem title

--- Algorithms/RemoveNthNodeFromEndOfList/removeNthNodeFromEndOfList.cc
+++ Algorithms/RemoveNthNodeFromEndOfList/removeNthNodeFromEndOfList.cc
@@ -1,6 +1,9 @@
 /*
 copyright xu(xusiwei1236@163.com), all right reserved.
 
+Remove Nth Node From End of List
+=================================
+
 Given a linked list, remove the nth node from the end of list and return its head.
 
 For example,
