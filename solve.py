a9d5df6650e29a225027352a411bf69bec0bce5d
xusiwei1236 <xusiwei1236@163.com>
2016-02-17 23:58:06 +0800

    auto create solution file from question page

--- solve.py
+++ solve.py
@@ -1,16 +1,58 @@
 #!/usr/bin/env python
 
+import re
 import os
 import os.path
 import sys
 import string
+import urllib
+from HTMLParser import HTMLParser
+from operator import __mod__
 
-if len(sys.argv) == 1:
-	print "Usage:", sys.argv[0], "Problem Title"
-	exit(-1)
+class QuestionParser(HTMLParser):
+	DEBUG = False
 
-newdir = "./Algorithms/"
-fname = ""
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.start_title_flag = None
+		self.start_content_flag = None
+		self.question_title = ''
+		self.question_content = ''
+
+	def handle_starttag(self, tag, attrs):
+		if self.DEBUG:
+			print 'start TAG:', tag, ', ATTRS:', attrs
+		if tag == 'div' and ('class', 'question-title') in attrs:
+			self.start_title_flag = True
+		elif tag == 'div' and ('class', 'question-content') in attrs:
+			self.start_content_flag = True
+
+	def handle_endtag(self, tag):
+		if self.DEBUG:
+			print 'end TAG:', tag
+		if self.start_title_flag and tag == 'div':
+			self.start_title_flag = False
+		elif self.start_content_flag and tag == 'div':
+			self.start_content_flag = False
+
+	def handle_data(self, data):
+		if self.DEBUG:
+			print 'DATA:', data
+		if self.start_title_flag:
+			self.question_title += data
+		elif self.start_content_flag:
+			self.question_content += data
+
+	def get_question_title(self):
+		t = self.question_title
+		return t[t.find('. ')+1: t.find('My Submissions')].strip()
+
+	def get_question_content(self):
+		c = self.question_content.replace('\r', '')
+		return c[:c.find('Subscribe to see which companies asked this question')].strip()
+
+basedir = "./Algorithms/"
+filename = ""
 suffix = ".cc"
 
 template = '''/*
@@ -19,7 +61,7 @@ copyright xu(xusiwei1236@163.com), all right reserved.
 {0}
 {1}
 
-
+{2}
 */
 
 int main(int argc, char* argv[])
@@ -28,41 +70,45 @@ int main(int argc, char* argv[])
 }}
 '''
 
-title = ""
-for i in range(1, len(sys.argv)):
-	if i != len(sys.argv)-1:
-		title = title + sys.argv[i] + " "
-	else:
-		title = title + sys.argv[i]
-	
-content = template.format(title, "=" * (1+len(title)))
-#print "prepare content:\n", content
 
-for i in range(1, len(sys.argv)):
-	w = sys.argv[i].replace("'", "")
-	newdir = newdir + string.capitalize(w[0]) + w[1:]
-	first = string.lower(w[0]) if i == 1 else string.capitalize(w[0])
-	fname = fname + first + w[1:]
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print "Usage:", sys.argv[0], "Problem-URL"
+		exit(-1)
 
-fname = newdir + "/" + fname + suffix
+	# fetch question page
+	page = urllib.urlopen(sys.argv[1]).read()
 
-# exists ?
-if os.path.exists(newdir):
-	print "dir:", newdir, "exists!"
-	exit(-2)
+	# parsing question
+	parser = QuestionParser()
+	parser.feed(page)
 
-# crate dir
-print "create dir:", newdir
-os.mkdir(newdir)
+	# format by template
+	title = parser.get_question_title()
+	split = '=' * (len(title)+1)
+	content = template.format(title, split, parser.get_question_content())
+	print 'content ready:\n', content
 
-# crate file
-print "create file:", fname
-fo = file(fname, 'w')
-if fo == None:
-	print "touch file:", fname, "failed!"
-	exit(-3)
+	# prepare filename
+	twords = title.split(' ')
+	for i in range(0, len(twords)):
+		w = twords[i].replace("'", "")
+		basedir = basedir + string.capitalize(w[0]) + w[1:]
+		first = string.lower(w[0]) if i == 1 else string.capitalize(w[0])
+		filename = filename + first + w[1:]
+	filename = basedir + "/" + filename + suffix
 
-fo.write(content)
+	# check directory exists ?
+	if os.path.exists(basedir):
+		print "dir:", basedir, "exists!"
+		exit(-2)
+	print "create dir:", basedir
+	os.mkdir(basedir)
 
-
-
+	# write to file
+	print "create file:", filename
+	fo = file(filename, 'w')
+	if fo == None:
+		print "touch file:", filename, "failed!"
+		exit(-3)
+	fo.write(content)
